2f8d87d78d80c20569671c39d50ec8372392d9f3
Basavaraj Kalloli <basavarajkalloli@gmail.com>
2025-05-09 18:29:30 +0100

    Initial commit: Basic Flask app with SQLAlchemy integration

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return 'Hello, World with SQLAlchemy!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
